effd70a042dddfcd0968e27b44677ae1c7d62494
Rodney Santiago <rodney.santiago@ups.com>
2022-10-10 13:57:01 -0400

    Before using Fody and ToPropertyEx

--- WpfApp1/MainWindowViewModel.cs
+++ WpfApp1/MainWindowViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using ClassLibrary1CoreService;
 using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
 using System.Reactive;
 using System.Reactive.Linq;
 using System.Runtime.CompilerServices;
@@ -13,23 +12,36 @@ namespace WpfApp1
         public MainWindowViewModel()
         {
             AddNumbers = ReactiveCommand.CreateFromTask(AddNumbersHandler);
-            AddNumbers.ToPropertyEx(this, y => y.Sum);
-            //AddNumbers.ThrownExceptions.Subscribe(ex => this.Log().Warn("Error!", ex));
+            AddNumbers.ToProperty(this, y => y.Sum, out _sum);
 
-            this.WhenAnyValue(x => x.Input1, x => x.Input2)
+            this.WhenAnyValue(x => x.Input1, x => x.Input2,
+                (a, b) => !string.IsNullOrEmpty(a) && !string.IsNullOrEmpty(b))
                 .Select(_ => Unit.Default)
                 .InvokeCommand(AddNumbers);
         }
 
         private async Task<string> AddNumbersHandler()
         {
+            await Task.Delay(1000);
+
             var sum = await ArithmeticAPIClient.AddNumbersAsync(Input1, Input2);
-            
+
             return sum > 0 ? $"{sum}" : "";
         }
 
-        [Reactive]public string Input1 { get; set; }
-        [Reactive] public string Input2 { get; set; }
-        public string Sum { [ObservableAsProperty] get; }
+        private string _input1;
+        public string Input1
+        {
+            get => _input1;
+            set => this.RaiseAndSetIfChanged(ref _input1, value);
+        }
+        private string _input2;
+        public string Input2
+        {
+            get => _input2;
+            set => this.RaiseAndSetIfChanged(ref _input2, value);
+        }
+        private ObservableAsPropertyHelper<string> _sum;
+        public string Sum => _sum.Value;
     }
 }
